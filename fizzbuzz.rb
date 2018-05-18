d2808fa31c4984dd626d1e4e48147939b97bc3a6
Kathy Nguyen <kathyvng@gmail.com>
2018-05-18 05:34:12 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz (int)
-  if int == % 3 
+  if int % 3 == 0
     "Fizz"
-  elsif int == % 5
+  elsif int % 5 ==
     "Buzz"
-  elsif int == % 15
+  elsif int % 15 >= 0
     "FizzBuzz"
   else !(int % 3 == 0 || int % 5 == 0)
     nil
